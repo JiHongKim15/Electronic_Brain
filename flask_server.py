be4d3b7221ab7f4511e1aec43e737e14f00ce62c
KimBMBM <49440051+KimBMBM@users.noreply.github.com>
2020-01-16 15:22:30 +0900

    Create flask_server.py

---
+++ flask_server.py
@@ -0,0 +1,157 @@
+from flask import Flask, request, jsonify
+import json
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+
+# mysql 접속
+conn = pymysql.connect(host='45.119.146.152', port=1024, user='trivle', password='Trivle_96', db='trivle',
+                       use_unicode=True, charset='utf8')
+
+
+# IP address of NodeMCU
+address = 'http://192.168.0.6'
+
+response_dict = {"response": {
+"outputSpeech": {
+"text": "",
+"type": "PlainText"
+},
+"shouldEndSession": True
+},
+"sessionAttributes": {},
+"version": "1.0"
+}
+
+
+@app.route('/', methods=['POST'])
+def index():
+    print(type(request.data))
+    print(type(request.json))
+
+    request_json = request.json
+    intent = get_intent_from_request(request_json)
+
+    result = ''
+    
+    #장보기
+    #item은 dialogflow에서 목록을 받아와서 검색
+    if intent == '//장보기intent//':
+        result = Shopping(item)
+    #합계
+    elif intent == '//합계intent':
+        result = Sum()
+    elif intent == '//목록보기':
+        result = View_List()
+    else:
+        result = handle_fallback_intent()
+
+    response_dict['response']['outputSpeech']['text'] = result
+
+    return jsonify(json.dumps(response_dict))
+
+#장보기 main
+def Shopping(item):
+    namelist = Name_Crawling()
+    pricelist = Price_Crawling()
+    Plus_List()
+
+    return ""
+
+
+# 장바구니db 가져오기
+def Sum():
+    cur = conn.cursor()
+    sql = 'SELECT * from //장바구니db name//;'
+    cur.execute(sql)  # 쿼리 수행
+    conn.commit()
+
+    rows = cur.fetchall()  # 결과 가져옴(데이터타입: 튜플)
+
+    for i in rows:
+        for j in i:
+            # 가격을 가져와서 +   
+    
+    # 장바구니 초기화
+
+    reset_sql = "Truncate table //장바구니db name//"
+    cur.execute(reset_sql)  # 쿼리 수행
+    conn.commit()
+    return ""
+
+
+# 장바구니에 목록 추가
+def  Plus_List(item):
+    cur = conn.cursor()
+    
+    # 장바구니 db 가져옴
+    sql = 'SELECT * from //장바구니db name//;'
+    cur.execute(sql)  # 쿼리 수행
+    rows = cur.fetchall()  # 결과 가져옴(데이터타입: 튜플)
+
+    # 장바구니 db 이용하여 추가
+    # 라면 / 가격 형태
+
+    return intent
+
+# 리스트 보기
+def View_List():
+    cur = conn.cursor()
+    
+    # 장바구니 db 가져옴
+    sql = 'SELECT * from //장바구니db name//;'
+    cur.execute(sql)  # 쿼리 수행
+    rows = cur.fetchall()  # 결과 가져옴(데이터타입: 튜플)
+
+    viewlist[] = {}
+
+    cnt = 1
+    for i in rows:
+        for j in i:
+            # view list
+            # 1. [품목/가격]
+            # 2. [품목/가격]
+            # 3. [품목/가격]
+            # 위와 같은 형태로 viewlist 생성하여 출력
+            viewlist.append(cnt + ". " + j.품목 + "/" + j.가격 + "\n") #품목
+          
+
+
+    return viewlist
+
+# 크롤링
+def Name_Crawling(menu, sort):
+    # "asc" : 낮은 가격순
+    # "dsc" : 높은 가격순
+    url = "https://search.shopping.naver.com/search/all.nhn?origQuery=" + menu + "&pagingIndex=1&pagingSize=40&viewType=list&sort=price_" + sort + "&query=" + menu
+    response = requests.get(url)
+
+    data = response.text
+    soup = BeautifulSoup(data, 'html.parser')
+
+    attr_name = {'class': 'link'}
+    attr_price = {'class': 'num _price_reload', }
+
+    name = soup.find_all('a', attrs=attr_name)
+    price = soup.find_all('span', attrs=attr_price)
+
+    result_price = list(price)
+    real_price = []
+    for i in result_price:
+        # print(str(i)[63:-7])
+        real_price.append(str(i)[63:-7])
+
+    result_name = list(name)
+    real_name = []
+
+    for i in result_name:
+        title_idx = str(i).find("title")
+        # print(str(i)[title_idx+7:-4])
+        real_name.append(str(i)[title_idx + 7:-4])
+
+    return real_name, real_price
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
